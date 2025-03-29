434b63395fcad9df23e4c6306d697a586cb0d786
Emmanuel-Cuestas-Uribe <al222210660@gmail.com>
2025-03-28 21:20:17 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-        l mis you wendy 
+        l mis you wendy  qa
         </p>
       </header>
     </div>
